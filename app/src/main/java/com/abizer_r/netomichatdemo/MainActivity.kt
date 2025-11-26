54d86f58e00375270822c8f88321b0c2c6a59acb
rabiz <rabizeridris@gmail.com>
2025-11-26 21:00:51 +0530

    fix insets settings upon ime (keyboard) changes

--- app/src/main/java/com/abizer_r/netomichatdemo/MainActivity.kt
+++ app/src/main/java/com/abizer_r/netomichatdemo/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -319,7 +320,9 @@ fun ChatCoreScreen(
 
         // Input row
         Row(
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .imePadding()
         ) {
             OutlinedTextField(
                 modifier = Modifier.weight(1f),
